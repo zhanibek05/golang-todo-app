10f86d0f4b6b301c3467aa5b3f308e39264b1ddf
zhanibek05 <beisenov.zhanibek@bk.ru>
2025-02-21 22:45:34 +0500

    added healthcheck route

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -1,12 +1,20 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 type Handler struct {
 }
 
 func (h *Handler) InitRoutes() *gin.Engine {
 	router := gin.New()
+
+	// Health check route
+	router.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"status": "ok"})
+	})
 
 	auth := router.Group("/auth")
 	{
