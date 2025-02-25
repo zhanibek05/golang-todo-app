47d19365b57ee3a0feddd4e541737e7949132e20
zhanibek05 <beisenov.zhanibek@bk.ru>
2025-02-25 18:15:46 +0500

    added configs

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/spf13/viper"
 	"github.com/zhanibek05/golang-todo-app"
 	"github.com/zhanibek05/golang-todo-app/pkg/database"
 	"github.com/zhanibek05/golang-todo-app/pkg/handler"
@@ -9,13 +10,22 @@ import (
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error loading config: %s", err.Error())
+	}
 	db := database.NewDatabase()
 	services := service.NewService(db)
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo_app.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatal("Server Error", err.Error())
 	}
 
+}
+
+func initConfig() error {
+	viper.AddConfigPath("./configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
